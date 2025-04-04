63d9df6fc821e783f940c71926359ad896e86233
Marcin Wąsowski <marcin.wasowski@gmail.com>
2025-04-04 12:30:18 +0200

    Add basic docstring

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from pathlib import Path
 
 
 def ls(path: Path) -> Iterable[Path]:
+    """
+    Traverse a given path, yielding paths of files and directories
+    """
     # nice-to-have: change to match
     path = Path(path)
     if path.is_dir():
