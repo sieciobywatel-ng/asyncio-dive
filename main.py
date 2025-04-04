d6646c0fff5cbd2eb609928fc4a992e4cbd627e0
Marcin Wąsowski <marcin.wasowski@gmail.com>
2025-04-04 11:55:24 +0200

    Sanity checks

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from typing import Iterable
+
+# import asyncio
+from pathlib import Path
+
+
+def traverse(directory: Path) -> Iterable[Path]:
+    pass
+
+
 def main():
     print("Hello from asyncio-dive!")
 
---
+++ test_sanity.py
@@ -0,0 +1,12 @@
+import os
+import main
+
+def test_main_py_can_be_run_from_os():
+    error = os.system('python main.py')
+    assert not rror
+
+
+def test_main_works():
+    main.main()
+    assert True
+
