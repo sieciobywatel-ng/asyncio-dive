d82c35640b8b7ce2bb0882b01027e8e2b6cda1d6
Marcin Wąsowski <marcin.wasowski@gmail.com>
2025-04-08 04:59:34 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import os
+
 from typing import Generator
-from typing import Union
+from pathlib import Path
 
 # import asyncio
-from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=getattr(
+        logging,
+        os.environ.get("LOG_LEVEL", '').upper(),
+        logging.ERROR,
+    ),
+)
+
+logger = logging.getLogger(__file__)
+
+path = Path('/dev/cpu')
+# path = '.venv/bin'
 
 
 def ls(path: Path) -> Generator[Path, None, None]:
@@ -15,17 +30,16 @@ def ls(path: Path) -> Generator[Path, None, None]:
     path: Path = Path(path) # type: ignore[no-redef]
     yield path
     if path.is_dir():
+        logger.debug(" * Traversing: %s", path)
         for child in path.iterdir(): # child: Path
             yield from ls(child)
 
-def main() -> None:
-    # procs should exist on every POSIX system and not be too long
-    path = Path('/dev/cpu')
-    processors: Generator[Path] = ls(path)
-    for cpu in processors:
-        print(cpu)
-    print("Hello from asyncio-dive!")
 
+def main():
+    # procs should exist on every POSIX system and not be too long
+    cpu = list(ls(path))
+    for found in cpu:
+        print(f" ls: {found}")
 
 if __name__ == "__main__":
     main()
