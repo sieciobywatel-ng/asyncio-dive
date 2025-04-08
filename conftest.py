df612dec44f7e77491251050d075bbd4cbb835d9
Marcin Wąsowski <marcin.wasowski@gmail.com>
2025-04-08 04:43:24 +0200

    Move fixtures to conftest

--- conftest.py
+++ conftest.py
@@ -1,3 +1,8 @@
+import os
+import shutil
+
+import pytest
+
 def pytest_configure(config):
     plugin = config.pluginmanager.getplugin('mypy')
     # plugin.mypy_argv.append('--check-untyped-defs')
@@ -5,3 +10,40 @@ def pytest_configure(config):
     plugin.mypy_argv.append('test_*.py')
     plugin.mypy_argv.append('--exclude')
     plugin.mypy_argv.append('conftest.py')
+
+
+
+@pytest.fixture()
+def tree(tmp_path):
+    # /
+    #     tree/
+    #     ├── bar
+    #     ├── egg
+    #     └── foo
+    #         └── bar
+    #             └── spam
+
+    root = tmp_path / 'tree'
+    foo = root / 'foo'
+    bar = root / 'bar'
+    foobar = foo / 'bar'
+
+    spam = foobar / 'spam'
+    egg = root / 'egg'
+
+    directories =[root, foo, foobar]
+    files = [spam, egg, bar]
+
+    for d in directories:
+        d.mkdir(parents=True, exist_ok=True)
+
+    for f in files:
+        f.touch(exist_ok=True)
+
+    os.system(f'tree {tmp_path}')
+    for f in [*directories, *files]:
+        assert f.exists()
+
+    yield root
+    # cleanup
+    shutil.rmtree(root)
--- test_ls.py
+++ test_ls.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 import pytest
 
@@ -34,39 +33,3 @@ def test_ls_is_able_to_traverse(tree):
     listed = [os.path.basename(p) for p in results]
     for name in expected:
         assert name in listed
-
-
-@pytest.fixture()
-def tree(tmp_path):
-    # /
-    #     tree/
-    #     ├── bar
-    #     ├── egg
-    #     └── foo
-    #         └── bar
-    #             └── spam
-
-    root = tmp_path / 'tree'
-    foo = root / 'foo'
-    bar = root / 'bar'
-    foobar = foo / 'bar'
-
-    spam = foobar / 'spam'
-    egg = root / 'egg'
-
-    directories =[root, foo, foobar]
-    files = [spam, egg, bar]
-
-    for d in directories:
-        d.mkdir(parents=True, exist_ok=True)
-
-    for f in files:
-        f.touch(exist_ok=True)
-
-    os.system(f'tree {tmp_path}')
-    for f in [*directories, *files]:
-        assert f.exists()
-
-    yield root
-    # cleanup
-    shutil.rmtree(root)
